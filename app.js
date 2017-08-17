07e5a1ef4d96e3e87c159909a814b95bfd022a2f
lockykeaney <lockykeaney@gmail.com>
2017-08-17 13:52:27 +1000

    env mongolab

--- app.js
+++ app.js
@@ -14,12 +14,24 @@ app.use(bodyParser.json());
 
 // database configuration ===============================================================
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yak');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/yak');
+
+
+const env = process.env.NODE_ENV;
+
+// if(env === 'development') {
+//   console.log(env);
+//   mongoose.connect('mongodb://localhost/yak');
+// } else if(env === 'production') {
+//   mongoose.connect('mongodb://lockykeaney:17Gardiner@ds013848.mlab.com:13848/mysterious-citadel');
+//   console.log(env);
+// }
+
 mongoose.Promise = require('bluebird');
 
 app.use(require('./routes'));
 
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('The magic happens on port ' + port + env);
 
 module.exports = app;
